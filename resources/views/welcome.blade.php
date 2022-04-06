9969403a4807045bf1b6faafb18457df7c3e2443
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-04-06 13:58:19 +0700

    abc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,9 @@
             }
         </style>
     </head>
+
+{{--    aaaaaaaaaaa   --}}
+
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
